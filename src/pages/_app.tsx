694cbb175dd7d44b5b6ee784c9f5ea348ba71b97
azer <azerprojects@gmail.com>
2021-03-05 14:50:28 +0700

    + switch theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,35 +1,25 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import App, { AppProps, AppContext } from 'next/app';
+import { AppProps } from 'next/app';
 import { CssBaseline, ThemeProvider } from '@material-ui/core';
-import { CookiesProvider } from 'react-cookie';
+import { CookiesProvider, useCookies } from 'react-cookie';
 import { dark, light } from '@styles/theme';
 
-export default function MainApp({
-  Component,
-  pageProps,
-  theme,
-}: PropertyTypes) {
+export default function MainApp(properties: AppProps) {
   return (
     <CookiesProvider>
-      <ThemeProvider theme={theme === 'light' ? light : dark}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <ThemeComponent {...properties} />
     </CookiesProvider>
   );
 }
 
-MainApp.getInitialProps = async (context: AppContext) => {
-  const properties = await App.getInitialProps(context);
-  const cookie = context.ctx.req?.headers.cookie;
-  const theme = cookie?.includes('theme=dark') ? 'dark' : 'light';
-  return {
-    pageProps: properties.pageProps,
-    theme,
-  };
-};
+function ThemeComponent({ Component, pageProps }: AppProps) {
+  const [cookies] = useCookies(['theme']);
 
-interface PropertyTypes extends AppProps {
-  theme: string;
+  return (
+    <ThemeProvider theme={cookies.theme === 'dark' ? dark : light}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import { Switch, Container, Button } from '@material-ui/core';
+import { useCookies } from 'react-cookie';
 
 export default function HomePage(): JSX.Element {
-  return <h1>Animer</h1>;
+  const [cookies, setCookie] = useCookies(['theme']);
+  const [toggleDarkTheme, setToggleDarkTheme] = React.useState(
+    cookies.theme === 'dark'
+  );
+  return (
+    <Container>
+      <Switch value={toggleDarkTheme} onChange={onThemeChanged} />
+      <Button variant="contained" color="primary">
+        I am a button
+      </Button>
+    </Container>
+  );
+
+  function onThemeChanged() {
+    const dark = !toggleDarkTheme;
+    const theme = dark ? 'dark' : 'light';
+    setCookie('theme', theme);
+    setToggleDarkTheme(dark);
+  }
 }
