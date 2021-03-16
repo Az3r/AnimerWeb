3fbbb45782e1a24812008c61cbd7bc5bd957a03d
azer <azerprojects@gmail.com>
2021-03-16 18:32:45 +0700

    feat(api): remove firebase calls from AuthApi function
    seperate between api call and firebase call for ease of testing
    
    BREAKING_CHANGE

--- __test__/libs/api.test.ts
+++ __test__/libs/api.test.ts
@@ -2,90 +2,82 @@
  * @jest-environment node
  */
 import { createToken } from '@utils/oauth';
-import { updateUserAuth } from '@pages/callback/auth';
 import {
   randomInt,
   randomString,
   randomAccessTokenResponse,
   createMockAuthApi,
-  createMockFirestoreAuth,
 } from '@__test__/utils';
 
-test('should be base64-url encoded', () => {
-  const token = createToken();
-  const regex = new RegExp(/^[\w-]+$/);
-  expect(token).toMatch(regex);
-});
-
-test('should receive AccessTokenResponse object with 2xx status code', async () => {
-  const response = randomAccessTokenResponse();
-  const codeVerifier = createToken();
-
-  const { api, mockAxios } = createMockAuthApi();
-  mockAxios.onPost('/token').reply(2 * 100 + randomInt(0, 99), response);
-
-  const data = await api.token(randomString(), codeVerifier);
-  expect(data).toMatchObject(response);
-});
-
-test('should fail when recieves invalid response data', async () => {
-  const response = {
-    random: 1,
-    invalid: createToken(),
-    data: '2',
-  };
-  const codeVerifier = createToken();
-
-  const { api, mockAxios } = createMockAuthApi();
-  mockAxios.onPost('/token').reply(2 + randomInt(0, 99), response);
-
-  const task = api.token(randomString(), codeVerifier);
-  await expect(task).rejects.toThrow();
-});
-
-test('should fail when recieves status code other than 2xx', async () => {
-  const hundreds = [1, 3, 4, 5];
-
-  const { api, mockAxios } = createMockAuthApi();
-
-  const tasks = hundreds.map((hundred) => {
-    const status = hundred * 100 + randomInt(0, 99);
-    mockAxios.onPost('/token').reply(status, createToken());
-    return api.token(randomString(), createToken());
+describe('AuthApi', () => {
+  test('should be base64-url encoded', () => {
+    const token = createToken();
+    const regex = new RegExp(/^[\w-]+$/);
+    expect(token).toMatch(regex);
   });
 
-  tasks.forEach(async (task) => {
+  test('should receive AccessTokenResponse object with 2xx status code', async () => {
+    const response = randomAccessTokenResponse();
+    const codeVerifier = createToken();
+
+    const { api, mockAxios } = createMockAuthApi();
+    mockAxios.onPost('/token').reply(2 * 100 + randomInt(0, 99), response);
+
+    const data = await api.token(randomString(), codeVerifier);
+    expect(data).toEqual(response);
+  });
+
+  test('should fail when recieves invalid response data', async () => {
+    const response = {
+      random: 1,
+      invalid: createToken(),
+      data: '2',
+    };
+    const codeVerifier = createToken();
+
+    const { api, mockAxios } = createMockAuthApi();
+    mockAxios.onPost('/token').reply(2 + randomInt(0, 99), response);
+
+    const task = api.token(randomString(), codeVerifier);
     await expect(task).rejects.toThrow();
   });
-});
 
-test('should update auth document', async () => {
-  // store user's token
-  const token = randomAccessTokenResponse();
-  const state = createToken();
-  const code = createToken();
-  const result = {
-    ...token,
-    codeChallenge: createToken(),
-    codeVerifier: createToken(),
-  };
-  const database = createMockFirestoreAuth();
-  await database.create(state, result.codeVerifier, result.codeChallenge);
+  test('should fail when recieves status code other than 2xx', async () => {
+    const hundreds = [1, 3, 4, 5];
 
-  const { api, mockAxios } = createMockAuthApi();
-  mockAxios.onPost('/token').reply(200, token);
+    const { api, mockAxios } = createMockAuthApi();
 
-  await updateUserAuth(state, code, database, api);
-  const data = await database.get(state);
-  expect(data).toMatchObject(result);
-});
+    const tasks = hundreds.map((hundred) => {
+      const status = hundred * 100 + randomInt(0, 99);
+      mockAxios.onPost('/token').reply(status, createToken());
+      return api.token(randomString(), createToken());
+    });
 
-test('should open new window when user request authorization', async () => {
-  Object.defineProperty(global, 'window', {
-    value: { open: jest.fn() },
-    writable: true,
+    tasks.forEach(async (task) => {
+      await expect(task).rejects.toThrow();
+    });
   });
-  const { api } = createMockAuthApi();
-  api.authorize(createToken(), createToken());
-  expect(window.open).toBeCalled();
+
+  test('should return exact AccessTokenResponse', async () => {
+    // store user's token
+    const token = randomAccessTokenResponse();
+    const state = createToken();
+    const code = createToken();
+
+    const { api, mockAxios } = createMockAuthApi();
+    mockAxios.onPost('/token').reply(200, token);
+    const result = await api.token(state, code);
+
+    expect(result).toEqual(token);
+  });
+
+  test('should open new window when user request authorization', async () => {
+    Object.defineProperty(global, 'window', {
+      value: { open: jest.fn() },
+      writable: true,
+    });
+    const { api } = createMockAuthApi();
+    api.authorize(createToken(), createToken());
+    expect(window.open).toBeCalled();
+  });
 });
